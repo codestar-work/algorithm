62799eddde260b62cdb89ab56a6ce6548da55d0f
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2015-06-25 22:56:22 +0700

    Update

---
+++ permute.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int count = 0;
+void permute(char *s, int k) {
+	if (k == 0) {
+		count++;
+		printf("%4d %s\n", count, s);
+	} else {
+		for (int i = k; i >= 0; i--) {
+			char t;
+			t = s[i]; s[i] = s[k]; s[k] = t;
+			permute(s, k-1);
+			t = s[i]; s[i] = s[k]; s[k] = t;
+		}
+	}
+}
+
+char s[26];
+int main() {
+	strcpy(s, "ABCD");
+	permute(s, strlen(s)-1);
+	return 0;
+}
--- permute.js
+++ permute.js
@@ -4,14 +4,14 @@ function permute(s, k) {
 		count++;
 		console.log(count + ' ' + s);
 	} else {
-		for (var i = k-1; i >= 0; i--) {
+		for (var i = k; i >= 0; i--) {
 			var tmp;
-			tmp = s[k-1]; s[k-1] = s[i]; s[i] = tmp;
+			tmp = s[k]; s[k] = s[i]; s[i] = tmp;
 			permute(s, k-1);
-			tmp = s[k-1]; s[k-1] = s[i]; s[i] = tmp;
+			tmp = s[k]; s[k] = s[i]; s[i] = tmp;
 		}
 	}
 }
 
 var a = ['A','B','C','D'];
-permute(a, a.length);
+permute(a, a.length-1);
