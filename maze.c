fa5a838dce33ed5b278ecd3c46899cc07b84f0fa
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2015-06-26 13:46:06 +0700

    Update

--- maze.c
+++ maze.c
@@ -75,9 +75,90 @@ void dfs() {
 	search(startRow, startCol, goalRow, goalCol);
 }
 
+struct item {
+	int row;
+	int col;
+	int parent;
+};
+struct item queue[MAX*MAX];
+int front = 0;
+int rear  = 0;
+void putQueue(int row, int col, int parent) {
+	queue[rear].row = row;
+	queue[rear].col = col;
+	queue[rear].parent = parent;
+	rear++;
+}
+
+struct item *getQueue() {
+	front++;
+	return &(queue[front-1]);
+}
+
+int isQueueEmpty() {
+	return rear == front;
+}
+
+void bfs() {
+	int startRow = -1, startCol = -1, goalRow = -1, goalCol = -1;
+	for (int i = 0; maze[i][0] != 0; i++) {
+		for (int j = 0; maze[i][j] != 0; j++) {
+			if (maze[i][j] == 'R') {
+				startRow = i;
+				startCol = j;
+			}
+			if (maze[i][j] == 'X') {
+				goalRow  = i;
+				goalCol  = j;
+			}
+		}
+	}
+	maze[startRow][startCol] = 'O';
+	maze[goalRow ][goalCol ] = ' ';
+	putQueue(startRow, startCol, -1);
+
+	while (!isQueueEmpty()) {
+		struct item *current = getQueue();
+		printf("%d %d\n", current->row, current->col);
+		if (current->row == goalRow && current->col == goalCol) {
+			int path = front-1;
+			for (int i = 0; maze[i][0] != 0; i++) {
+				for (int j = 0; maze[i][j] != 0; j++) {
+					if (maze[i][j] == 'O') {
+						maze[i][j]  = ' ';
+					}
+				}
+			}
+
+			while (path != -1) {
+				maze[queue[path].row][queue[path].col] = 'O';
+				path = queue[path].parent;
+			}
+			printMaze();
+			break;
+		} else {
+			if (maze[current->row-1][current->col] == ' ') {
+				maze[current->row-1][current->col]  = 'O';
+				putQueue(current->row-1, current->col, front-1);
+			}
+			if (maze[current->row+1][current->col] == ' ') {
+				maze[current->row+1][current->col]  = 'O';
+				putQueue(current->row+1, current->col, front-1);
+			}
+			if (maze[current->row][current->col-1] == ' ') {
+				maze[current->row][current->col-1]  = 'O';
+				putQueue(current->row, current->col-1, front-1);
+			}
+			if (maze[current->row][current->col+1] == ' ') {
+				maze[current->row][current->col+1]  = 'O';
+				putQueue(current->row, current->col+1, front-1);
+			}
+		}
+	}
+}
+
 int main() {
 	readMaze();
-	dfs();
-	// printMaze();
+	bfs();
 	return 0;
 }
